b7f71ae48e4c0eaa8200248e2dfddee1d2801340
vidurshan96 <vidurshan1996@gmail.com>
2019-12-31 23:01:36 +0530

    front page aand area

--- app/src/main/java/com/vidu/tilecalculator/MainActivity.java
+++ app/src/main/java/com/vidu/tilecalculator/MainActivity.java
@@ -1,8 +1,11 @@
 package com.vidu.tilecalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton btn1 = (ImageButton) findViewById(R.id.btn_wall);
+        ImageButton floor = (ImageButton) findViewById(R.id.btn_floor);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(),walltile.class);
+                startActivity(startIntent);
+
+
+            }
+        });
+        floor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(),floortile.class);
+                startActivity(startIntent);
+
+
+            }
+        });
+
+
+
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/vidu/tilecalculator/walltile.java
@@ -0,0 +1,36 @@
+package com.vidu.tilecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class walltile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_walltile);
+
+        Button cal_btn = (Button) findViewById(R.id.cal_btn);
+        cal_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EditText width_txt = (EditText) findViewById(R.id.width_txt);
+                EditText  length_txt = (EditText) findViewById(R.id.length_txt);
+                TextView area_txt = (TextView) findViewById(R.id.area);
+                int length = Integer.parseInt(length_txt.getText().toString());
+                int width = Integer.parseInt(width_txt.getText().toString());
+
+
+                int area = length * width;
+
+                area_txt.setText(area +"");
+            }
+        });
+    }
+}
