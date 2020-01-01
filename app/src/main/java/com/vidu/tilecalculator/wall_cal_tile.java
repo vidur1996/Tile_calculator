d4538256aae0717a88c7d52374ea6eb05b4a72fd
vidurshan96 <vidurshan1996@gmail.com>
2020-01-01 21:52:55 +0530

    design finish

---
+++ app/src/main/java/com/vidu/tilecalculator/wall_cal_tile.java
@@ -0,0 +1,9 @@
+package com.vidu.tilecalculator;
+
+public class wall_cal_tile
+{
+    public static int cal(char t,int hight,int breath ,int ori)
+    {
+        return 0;
+    }
+}
--- app/src/main/java/com/vidu/tilecalculator/walltile.java
+++ app/src/main/java/com/vidu/tilecalculator/walltile.java
@@ -4,18 +4,56 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class walltile extends AppCompatActivity {
+
+    Spinner spin1;
+    Spinner spin2;
+    TextView area_txt1;
+    TextView area_txt2;
+    TextView tile_txt1;
+    TextView tile_txt2;
+    TextView declare;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_walltile);
 
+
+
+
+
         Button cal_btn = (Button) findViewById(R.id.cal_btn);
+        spin1 = (Spinner) findViewById(R.id.spin1);
+        spin2 = (Spinner) findViewById(R.id.spin2);
+        area_txt1 = (TextView) findViewById(R.id.area_txt);
+        area_txt2 = (TextView) findViewById(R.id.area_txt2);
+        tile_txt1 = (TextView) findViewById(R.id.tile_txt);
+        tile_txt2 = (TextView) findViewById(R.id.tile_txt2);
+        declare = (TextView) findViewById(R.id.declare);
+
+        area_txt1.setVisibility(TextView.INVISIBLE);
+        area_txt2.setVisibility(TextView.INVISIBLE);
+        tile_txt1.setVisibility(TextView.INVISIBLE);
+        tile_txt2.setVisibility(TextView.INVISIBLE);
+        declare.setVisibility(TextView.INVISIBLE);
+
+        ArrayAdapter<String> myadapter1  = new ArrayAdapter<String>(walltile.this
+                ,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.wall_size));
+        myadapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin1.setAdapter(myadapter1);
+
+        ArrayAdapter<String> myadapter2  = new ArrayAdapter<String>(walltile.this
+                ,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.orientation));
+        myadapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin2.setAdapter(myadapter2);
+
         cal_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -23,8 +61,12 @@ public class walltile extends AppCompatActivity {
                 EditText width_txt = (EditText) findViewById(R.id.width_txt);
                 EditText  length_txt = (EditText) findViewById(R.id.length_txt);
                 TextView area_txt = (TextView) findViewById(R.id.area);
+                TextView tile_txt = (TextView) findViewById(R.id.tile);
+                TextView appx_tile = (TextView) findViewById(R.id.appx_tile);
                 int length = Integer.parseInt(length_txt.getText().toString());
                 int width = Integer.parseInt(width_txt.getText().toString());
+                String text =  spin1.getSelectedItem().toString();
+                String ori = spin2.getSelectedItem().toString();
 
 
                 int area = length * width;
