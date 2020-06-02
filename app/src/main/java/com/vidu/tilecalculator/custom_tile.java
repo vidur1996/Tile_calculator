6ed397258cdf2031c36aa95efa18f9befae6edbf
vidurshan <vidurshan1996>
2020-06-02 12:37:55 +0530

    new ad

---
+++ app/src/main/java/com/vidu/tilecalculator/custom_tile.java
@@ -0,0 +1,14 @@
+package com.vidu.tilecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class custom_tile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_tile);
+    }
+}
